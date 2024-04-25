6edd94c8167ac1d6c7bfd412d07e0c32892f0826
MIKEKIMEU <muiamichael234@gmail.com>
2024-04-25 14:58:59 +0300

    Remove unnecessary files and dependencies

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.use("/api",(req,res,next)=>{
-    res.send("Hello World!");
-})
 
-app.listen(5000);
+mongoose.connect("mongodb+srv://admin:vvIzZQVXZoMVCBPR@cluster0.jls7c4s.mongodb.net/Blog?retryWrites=true&w=majority&appName=Cluster0"
+)
+.then(()=>app.listen(5000))
+.then(()=>
+console.log("Connected To Database and Listening to localhost:5000")
+)
+
+.catch((err)=>console.log(err));
+
+
+//password: vvIzZQVXZoMVCBPR
